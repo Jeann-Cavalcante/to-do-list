55275e2890d7a7e22bce637c5c05a82e39128452
Jean <jeanncavalcante@gmail.com>
2023-02-12 23:20:33 -0300

    adicionando .env no projeto

--- database/db.js
+++ database/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+require('dotenv').config()
 
 const connectToDb = () => {
   mongoose
     .connect(
-      "mongodb+srv://root:admin@todolist.00igg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      process.env.PRIVATE_KEY,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
